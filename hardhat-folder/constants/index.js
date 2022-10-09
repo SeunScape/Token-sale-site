f1751c33e0d2b99c70d19092330bde827cd8aecf
SeunScape <69199129+SeunScape@users.noreply.github.com>
2022-10-10 00:00:53 +0100

    first commit

---
+++ hardhat-folder/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xe0563A221cF81130B167A81CF138dc33CBe9A236";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
